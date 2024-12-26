5faeaaa8c9fc1537fb324107f1e9b7f156eb4900
demacedius <demacedius@gmail.com>
2024-12-26 19:58:11 +0100

    Menu animation

--- src/script/menu.js
+++ src/script/menu.js
@@ -1,3 +1,31 @@
+import gsap from "gsap";
+let isExpanded = false;
+let mm = gsap.matchMedia();
+
 document.querySelector('.burger')?.addEventListener('click', () => {
-    document.querySelector('.nav-link')?.classList.toggle('expanded');
-})
+    const navLinks = document.querySelector('.nav-link');
+
+    if (!isExpanded) {
+        navLinks.classList.add('expanded');
+        aniamtedMenuIn();
+    } else {
+        animatedMenuOut();
+        setTimeout(() => navLinks.classList.remove('expanded'), 500);
+    }
+
+    isExpanded = !isExpanded;
+});
+
+
+function aniamtedMenuIn() { 
+    mm.add("(max-width: 860px)", () => {
+        gsap.fromTo(".nav-link", { opacity: 0, y: -80 }, { opacity: 1, y: 0, transform: "scale(1)", duration: 0.5, ease: "power4.in" });
+    })
+}
+
+function animatedMenuOut() {
+    mm.add("(max-width: 860px)", () => {
+        
+        gsap.fromTo(".nav-link", { opacity: 1, y: 0 ,}, { opacity: 0, y: -80, duration: 0.5, ease: "power4.Out" });
+    })
+}
